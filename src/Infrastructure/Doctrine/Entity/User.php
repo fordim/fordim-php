ebe7a09439643f956b6377f0a6650abf9f09ffe2
fordim <dmitriy.formos@skyeng.ru>
2023-11-02 18:43:00 +0300

    Add User Entity

---
+++ src/Infrastructure/Doctrine/Entity/User.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Infrastructure\Doctrine\Entity;
+
+use App\Infrastructure\Repository\Doctrine\User\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
+
+    #[ORM\Column(length: 255)]
+    private string $login;
+
+    #[ORM\Column(length: 255)]
+    private string $password ;
+
+    #[ORM\Column(length: 255)]
+    private string $role;
+
+    #[ORM\Column(length: 255)]
+    private string $name;
+
+    #[ORM\Column(name: 'created_at')]
+    private \DateTimeImmutable $createdAt;
+
+    #[ORM\Column(name: 'updated_at')]
+    private \DateTimeImmutable $updatedAt;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getLogin(): string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): static
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): static
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): \DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
